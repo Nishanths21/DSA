77060a862f1ebc81deaea9e6ae7e8155d9b11147
Nishanths21 <nishanths1441@gmail.com>
2025-11-11 19:22:00 +0530

    changes

---
+++ String/sort-characterby-frequency.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+public: 
+    string frequencysort(string s){
+        int n=s.length();
+        unordered_map<char,int>counts;
+        for(char c:s){
+            counts[c]++;
+        }
+        vector<string>buckets[n+1,""];
+        for(auto const& [characters,frequency]:counts){
+            char repeated_char(frequency,characters);
+            buckets[frequency]+=repeated_char;
+        }
+        string result=" ";
+        for(int i=n;i>=0;i--){
+            if(!buckets[i].empty()){
+                result+=buckets[i];
+            }
+        }
+        return result;
+    }
+}
