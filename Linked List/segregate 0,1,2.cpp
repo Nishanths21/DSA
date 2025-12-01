096c21706a90c18c4350bab6ecd05fe1878d2ac6
Nishanths21 <nishanths1441@gmail.com>
2025-12-01 18:08:07 +0530

    11

--- Linked List/segregate 0,1,2.cpp
+++ Linked List/segregate 0,1,2.cpp
@@ -44,3 +44,53 @@ public:
         return newhead;
     }
 }
+
+//one more way of solving this problem is counting 0s,1s and 2s and then updating the original list accordingly.
+class Solution{
+public:
+    Node* sortnode(Node* head){
+        if(!head   || !head->next) return head;
+        int count0=0;
+        int coun1=0;
+        int count2=0;
+        Node* temp=head;
+        while(temp!=NULL){
+            if(temp->data==0){
+                count0++;
+            }
+            else if(temp->data==1){
+                count1++;
+            }
+            else{
+                count2++;
+            }
+            temp=temp->next;
+        }
+        temp=head;
+        while(temp!=NULL){
+            if(count0>0){
+                temp->data=0;
+                count0--;
+            }
+            else if(count1>0){
+                temp->data=1;
+                count1--;
+            }
+            else{
+                temp->data=2;
+                count2--;
+            }
+            temp=temp->next;
+        }
+        return head;
+    }
+};
+void printlist(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+};
+
